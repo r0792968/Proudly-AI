2e92acc4646d0cf24ed1be6635a5d69f01062632
matty1400 <matijs.eraly@live.be>
2023-05-23 16:58:47 +0200

    Update api.php

--- Proudly-BackEnd/routes/api.php
+++ Proudly-BackEnd/routes/api.php
@@ -26,28 +26,28 @@ Route::get('user/', [DeviceController::class, 'getUser']);
 
 
 
-Route::get('getIdByIndustry', [DeviceController::class, 'getIdByIndustry']);
+Route::get('company/filter/industry', [DeviceController::class, 'getIdByIndustry']);
 
-Route::get('getIdByHeadcount', [DeviceController::class, 'getIdByHeadcount']);
-Route::get('getIdByHeadquarters', [DeviceController::class, 'getIdByHeadquarters']);
-Route::get('getIdBySeniority', [DeviceController::class, 'getIdBySeniority']);
-Route::get('getIdByFunction', [DeviceController::class, 'getIdByFunction']);
-Route::get('getCompanySearchByUserId', [DeviceController::class, 'getCompanySearchByUserId']);
-Route::get('getPeopleSearchByUserId', [DeviceController::class, 'getPeopleSearchByUserId']);
-Route::get('getCompanyLeadsBySearchId', [DeviceController::class, 'getCompanyLeadsBySearchId']);
-Route::get('getPeopleLeadsBySearchId', [DeviceController::class, 'getPeopleLeadsBySearchId']);
-Route::get('getIndustryNames', [DeviceController::class, 'getIndustryNames']);
-Route::get('getHeadcount', [DeviceController::class, 'getHeadcount']);
-Route::get('getHeadquarters', [DeviceController::class, 'getHeadquarters']);
+Route::get('company/filter/headcount', [DeviceController::class, 'getIdByHeadcount']);
+Route::get('company/filter/headquarters', [DeviceController::class, 'getIdByHeadquarters']);
+Route::get('people/filter/seniority', [DeviceController::class, 'getIdBySeniority']);
+Route::get('people/filter/function', [DeviceController::class, 'getIdByFunction']);
+Route::get('company/searches', [DeviceController::class, 'getCompanySearchByUserId']);
+Route::get('people/searches', [DeviceController::class, 'getPeopleSearchByUserId']);
+Route::get('company/leads', [DeviceController::class, 'getCompanyLeadsBySearchId']);
+Route::get('people/leads', [DeviceController::class, 'getPeopleLeadsBySearchId']);
+Route::get('company/filter/allindustries', [DeviceController::class, 'getIndustryNames']);
+Route::get('company/filter/allheadcounts', [DeviceController::class, 'getHeadcount']);
+Route::get('company/filter/allheadquarters', [DeviceController::class, 'getHeadquarters']);
 
 
 
 //POST REQUESTS
 Route::post('user', [DeviceController::class, 'postUser']);
-Route::post('newPeopleLeads', [DeviceController::class, 'newPeopleLeads']);
-Route::post('newCompanyLeads', [DeviceController::class, 'newCompanyLeads']);
-Route::post('newPeopleSearch', [DeviceController::class, 'newPeopleSearch']);
-Route::post('newCompanySearch', [DeviceController::class, 'newCompanySearch']);
+Route::post('people/leads', [DeviceController::class, 'newPeopleLeads']);
+Route::post('company/leads', [DeviceController::class, 'newCompanyLeads']);
+Route::post('people/searches', [DeviceController::class, 'newPeopleSearch']);
+Route::post('company/searches', [DeviceController::class, 'newCompanySearch']);
 Route::post('phantom/updateAndLaunch', [DeviceController::class, 'updateAndLaunch']);
 Route::post('phantom/fetcher', [DeviceController::class, 'fetcher']);
 
